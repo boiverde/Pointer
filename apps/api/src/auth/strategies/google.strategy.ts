5b3a3905710f5e2f9cadee96f358fee7670553ba
boiverde <aguiar.neves@hotmail.com>
2025-12-31 20:35:35 -0300

    Use ConfigService for Google Strategy

--- apps/api/src/auth/strategies/google.strategy.ts
+++ apps/api/src/auth/strategies/google.strategy.ts
@@ -2,15 +2,19 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-    constructor(private authService: AuthService) {
+    constructor(
+        private authService: AuthService,
+        private configService: ConfigService
+    ) {
         super({
-            clientID: process.env.GOOGLE_CLIENT_ID || 'place-holder',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'place-holder',
-            callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3001/auth/google/callback',
+            clientID: configService.get<string>('GOOGLE_CLIENT_ID') || 'place-holder',
+            clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET') || 'place-holder',
+            callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL') || 'http://localhost:3001/auth/google/callback',
             scope: ['email', 'profile'],
         });
     }
